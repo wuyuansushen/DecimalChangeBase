643b2b05154e18c7ff78a9d0fba8879ae9446450
wuyuansushen <819070864@qq.com>
2021-03-10 23:46:25 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DecimalChangeBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Number:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input Base Number(2-36):");
+            int baseNum = Convert.ToInt32(Console.ReadLine());
+
+            if (baseNum > 36 || baseNum < 2)
+            {
+                Console.WriteLine("Base Number error");
+                return;
+            }
+            else
+            {
+
+                List<int> output = new List<int>();
+
+                //Display
+                #region
+                Dictionary<int, char> outputMap = new Dictionary<int, char>();
+
+                char LessTen = '0';
+                if (baseNum < 10)
+                {
+                    for (int i = 0; i < baseNum; i++)
+                    {
+                        outputMap.Add(i, LessTen);
+                        LessTen++;
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        outputMap.Add(i, LessTen);
+                        LessTen++;
+                    }
+                    LessTen = 'A';
+                    for (int i = 10; i < baseNum; i++)
+                    {
+                        outputMap.Add(i, LessTen);
+                        LessTen++;
+                    }
+                }
+                #endregion
+
+                //Input Process and Half return
+                #region
+                if (input < baseNum)
+                {
+                    Console.WriteLine($"{outputMap[input]}");
+                    return;
+                }
+                else
+                {
+                    while (input >= baseNum)
+                    {
+                        int tmp = input % baseNum;
+                        output.Add(tmp);
+                        input = (input - tmp) / baseNum;
+                    }
+                    output.Add(input);
+                }
+                #endregion
+
+                //another half Output
+                for (int i = output.Count - 1; i >= 0; i--)
+                {
+                    Console.Write($"{outputMap[output[i]]}");
+                }
+            }
+        }
+    }
+}
